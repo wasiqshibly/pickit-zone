c52333af3dc1f01bba3d7b9627b6cf3c5ada352e
wasiqshibly <wasiqshiblyih@gmail.com>
2017-09-09 21:54:21 +0600

    Add files via upload

---
+++ EEPROM_integer_number_input_and_save_it_and_print_it.ino
@@ -0,0 +1,32 @@
+#include <EEPROM.h>
+int incomingByte = 0;   
+void setup() {
+        Serial.begin(9600);     // opens serial port, sets data rate to 9600 bps
+}
+
+void loop() {
+
+        // send data only when you receive data:
+        if (Serial.available() > 0) {
+                int red = Serial.parseInt();
+
+      
+                Serial.println(red,DEC);
+                EEPROM.write(9,red);
+                delay(1000);
+        }
+        else
+        {
+     int k=EEPROM.read(9);
+          Serial.println(k);
+          delay(1000);
+          
+        }
+}
+ 
+
+
+
+
+
+
