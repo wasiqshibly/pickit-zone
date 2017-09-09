4d8c76a82efd31333fb6ccdc39f631c3a7203aa3
wasiqshibly <wasiqshiblyih@gmail.com>
2017-09-09 22:18:54 +0600

    Add files via upload

---
+++ motor/pwm_MOTOR_LED__with_user_input_control.ino
@@ -0,0 +1,30 @@
+void setup() {
+pinMode(9,OUTPUT);
+Serial.begin(9650);
+
+}
+
+void loop() {
+
+   if (Serial.available() > 0) {
+                int p = Serial.parseInt();
+
+      
+                Serial.println(p,DEC);
+  if(p==1)
+  {
+  p+=90;
+analogWrite(9,p);
+  }
+  else if(p==2)
+  {
+
+  p+=250;
+analogWrite(9,p);
+    
+  }
+}
+  
+  // put your main code here, to run repeatedly:
+
+}
